837f2114ed0c79895c2965f8a3bc1993e45ec3fc
notsureprog <notsureprog@gmail.com>
2023-12-07 19:07:54 -0500

    Second Commit. Structuring.

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='PropertiesBoughtPage' component={PropertiesBoughtPage} />
+        <Stack.Screen name='PropertiesSoldPage' component={PropertiesSoldPage} />
+        <Stack.Screen name='PropertiesRentingPage' component={PropertiesRentingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
